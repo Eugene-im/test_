b579b8b14408769c0af6b86b0789ca3671423073
Eugene-im <em.92@mail.ru>
2018-06-19 13:44:48 +0300

    some thing better will be with slick

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
-
+document.addEventListener("DOMContentLoaded",setTimeout);
+setTimeout(click, 10000);
 
 function click(){
+    var          a = document.getElementById('cssload');
     var reg_button = document.getElementById('reg_b');
     var sig_button = document.getElementById('sig_b');
+    var reg = document.getElementById('reg');
+    var sig = document.getElementById('sig');
+    // var prog = document.getElementById('progress')
+    // var header = document.getElementsByTagName('header')[0];
+    // var main = document.getElementsByTagName('main')[0];
+    // var footer = document.getElementsByTagName('footer')[0];
+
+    a.setAttribute('class','fade');
+    setTimeout(a.setAttribute('class','hidden'),800);
+
+    // window.onscroll = function(){
+    //     document.getElementById('wr').scrollBy(0,1000);
+    //     prog.setAttribute('class','class-33');
+    // }
+
     sig_button.onclick = function(){
-        document.getElementById('reg').removeAttribute('class','hidden');
-        document.getElementById('sig').setAttribute('class','hidden');                
+        reg.removeAttribute('class','hidden');
+        sig.setAttribute('class','hidden');  
+        // window.scrollTo(0,1000);
+                      
     }
     reg_button.onclick = function(){
-        document.getElementById('reg').setAttribute('class','hidden');
-        document.getElementById('sig').removeAttribute('class','hidden');        
+        reg.setAttribute('class','hidden');
+        sig.removeAttribute('class','hidden');        
     }
-    console.log("reg and sig catched")
-
 };
-click();
