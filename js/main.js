e6247cf60281ef9c85dbf10473d2c2d8ed7cb65d
Eugene-im <em.92@mail.ru>
2018-06-19 15:36:39 +0300

    i think looks good

--- js/main.js
+++ js/main.js
@@ -1,33 +1,39 @@
-document.addEventListener("DOMContentLoaded",setTimeout);
+document.addEventListener("DOMContentLoaded", setTimeout);
 setTimeout(click, 10000);
 
-function click(){
-    var          a = document.getElementById('cssload');
-    var reg_button = document.getElementById('reg_b');
-    var sig_button = document.getElementById('sig_b');
-    var reg = document.getElementById('reg');
-    var sig = document.getElementById('sig');
-    // var prog = document.getElementById('progress')
-    // var header = document.getElementsByTagName('header')[0];
-    // var main = document.getElementsByTagName('main')[0];
-    // var footer = document.getElementsByTagName('footer')[0];
+function click() {
+  var a = document.getElementById("cssload");
+  var reg_button = document.getElementById("reg_b");
+  var sig_button = document.getElementById("sig_b");
+  var reg = document.getElementById("reg");
+  var sig = document.getElementById("sig");
 
-    a.setAttribute('class','fade');
-    setTimeout(a.setAttribute('class','hidden'),800);
+  a.setAttribute("class", "fade");
+  setTimeout(a.setAttribute("class", "hidden"), 800);
 
-    // window.onscroll = function(){
-    //     document.getElementById('wr').scrollBy(0,1000);
-    //     prog.setAttribute('class','class-33');
-    // }
+  sig_button.onclick = function() {
+    reg.removeAttribute("class", "hidden");
+    sig.setAttribute("class", "hidden");
+  };
+  reg_button.onclick = function() {
+    reg.setAttribute("class", "hidden");
+    sig.removeAttribute("class", "hidden");
+  };
+}
 
-    sig_button.onclick = function(){
-        reg.removeAttribute('class','hidden');
-        sig.setAttribute('class','hidden');  
-        // window.scrollTo(0,1000);
-                      
-    }
-    reg_button.onclick = function(){
-        reg.setAttribute('class','hidden');
-        sig.removeAttribute('class','hidden');        
-    }
-};
+$(".wrap").slick({
+  vertical: true,
+  verticalSwiping: true,
+  infinite: false,
+  prevArrow: "",
+  nextArrow:
+    '<span class="scroll-btn">\
+    <a href="#">\
+        <span class="mouse">\
+            <span>\
+            </span>\
+        </span>\
+        <p>drag me</p>\
+    </a>\
+</span>'
+});
